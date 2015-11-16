a2516953a66c3d8eaa8ac096230df7067b9142f5
Stino <Linksonder@gmail.com>
2015-11-16 08:42:13 +0100

    init

---
+++ MyBlog/MyBlog/MainVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog
+{
+    public class MainVM
+    {
+        private IPostRepository _postRepo;
+
+        public MainVM(IPostRepository postRepo)
+        {
+            this._postRepo = postRepo;
+        }
+    }
+}
---
+++ MyBlog/MyBlogStarter/ViewModels/NinjectServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogStarter.ViewModels
+{
+    public class NinjectServiceLocator
+    {
+        public MainViewModel Main
+        {
+            get
+            {
+                throw new NotImplementedException("Je moet de klasse NinjectServiceLocator nog afmaken");
+            }
+        }
+    }
+}
