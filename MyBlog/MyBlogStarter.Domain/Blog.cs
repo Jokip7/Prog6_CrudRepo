a97735998dd7e3ea592507b035abb2403d19b1b2
Stino <Linksonder@gmail.com>
2015-11-16 11:29:02 +0100

    Rename resources

--- MyBlog/MyBlogStarter.Domain/Blog.cs
+++ MyBlog/MyBlogStarter.Domain/Blog.cs
@@ -8,5 +8,14 @@ namespace MyBlogStarter.Domain
 {
     public class Blog
     {
+        public int Id { get; set; }
+
+        public String Title { get; set; }
+
+        public String Content { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public String Author { get; set; }
     }
 }
